3f646e292e71145c36905b9787e445737bc7ab24
Harsh Solanki <134034742+HS160@users.noreply.github.com>
2024-08-31 01:13:52 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,9 +1,12 @@
 print("\t\t\t Welcome to my BMI calculator\n\n\n")
-
 height = float(input("what is your height in meters : "))
 weight = float(input("what is your weight in kg : "))
-
-
 bmi = weight/(height*height)
-
 print(f"Your BMI is - {bmi:.2f}")
+
+if bmi < 18.5:
+    print("Under Weight!")
+elif bmi>= 18.5 and bmi<=25:
+    print("Normal Weight!")
+else:
+    print("Overweight!")
